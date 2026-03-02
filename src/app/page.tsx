50cc3a89fd3a1add7b6cfc74a94aa5cdd5347a97
SurajYadav8 <raosahab1e@gmail.com>
2026-03-02 05:01:56 +0000

    layout & Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import {Button} from "@/components/ui/button";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { api } from "../../convex/_generated/api";
+import { useQuery } from "convex/react";
 
 const X = () => {
+  const tasks = useQuery(api.tasks.get)
   return (
-    <div>
-      <Button >
-        Click me 
-      </Button>
+    <div className="flex flex-col gap-2 p-4">
+      {tasks?.map((task) => (
+        <div className="border rounded p-2 flex flex-col" key={task.id}>
+          <p> {task.text}</p>
+          <p> Is completed: {`${task.isCompleted}`}</p>
+        </div>
+      ))}
     </div>
 
   );
