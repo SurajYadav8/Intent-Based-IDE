db66ecb36c40309a049b6de5a356ffcca64ac01f
SurajYadav8 <raosahab1e@gmail.com>
2026-03-02 18:47:35 +0000

    convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { api } from "../../convex/_generated/api";
 import { useMutation, useQuery } from "convex/react";
 
 const X = () => {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
+  const projects = useQuery(api.project.get);
+  const createProject = useMutation(api.project.create);
 
   return (
     <div className="flex flex-col gap-2 p-4">
@@ -17,7 +17,7 @@ const X = () => {
       </Button>
       
       {projects?.map((project) => (
-        <div className="border rounded p-2 flex flex-col" key={project.id}>
+        <div className="border rounded p-2 flex flex-col" key={project._id}>
           <p> {project.name}</p>
           <p> Owner Id: {project.ownerId}</p>
         </div>
