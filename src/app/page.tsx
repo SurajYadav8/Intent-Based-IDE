6f1f14c05848ece5a2b547be3b445f9df34775fa
SurajYadav8 <raosahab1e@gmail.com>
2026-03-02 18:44:32 +0000

    schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,24 @@
 
 import { Button } from "@/components/ui/button";
 import { api } from "../../convex/_generated/api";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 const X = () => {
-  const tasks = useQuery(api.tasks.get)
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+
   return (
     <div className="flex flex-col gap-2 p-4">
-      {tasks?.map((task) => (
-        <div className="border rounded p-2 flex flex-col" key={task.id}>
-          <p> {task.text}</p>
-          <p> Is completed: {`${task.isCompleted}`}</p>
+      <Button onClick={() => createProject({
+        name: "New Project"
+      })}>
+        Add new 
+      </Button>
+      
+      {projects?.map((project) => (
+        <div className="border rounded p-2 flex flex-col" key={project.id}>
+          <p> {project.name}</p>
+          <p> Owner Id: {project.ownerId}</p>
         </div>
       ))}
     </div>
