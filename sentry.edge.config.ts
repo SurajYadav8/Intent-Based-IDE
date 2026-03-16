de1ff5be5482aff17df0031709121f85c97731b9
SurajYadav8 <raosahab1e@gmail.com>
2026-03-16 17:21:45 +0000

    05: error tracking

---
+++ sentry.edge.config.ts
@@ -0,0 +1,25 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7ea24f0bb4d2c88279157d28e0c4b80d@o4511053407191040.ingest.us.sentry.io/4511053418266624",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.consoleLoggingIntegration({
+      levels: ["log", "warn", "error"],
+    }),
+  ],
+});
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -9,9 +9,9 @@ export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
   async ({ event, step }) => {
-    const { prompt } = event.data as { prompt: string;};
+    const { prompt } = event.data as { prompt: string; };
 
-    const  urls = await step.run("extract-urls", async () => {
+    const urls = await step.run("extract-urls", async () => {
       return prompt.match(URL_REGEX) ?? [];
     }) as string[];
 
@@ -33,10 +33,25 @@ export const demoGenerate = inngest.createFunction(
       : prompt;
 
     await step.run("generate-text", async () => {
-        return await generateText({
-            model: google('gemini-2.5-flash'),
-            prompt: finalPrompt,
-        })
+      return await generateText({
+        model: google('gemini-2.5-flash'),
+        prompt: finalPrompt,
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      })
     });
   },
+);
+
+export const demoError = inngest.createFunction(
+  { id: "demo-error" },
+  { event: "demo/error" },
+  async ({ step }) => {
+    await step.run("fail", async () => {
+      throw new Error("Inngest error: Background job failed!");
+    });
+  }
 );
